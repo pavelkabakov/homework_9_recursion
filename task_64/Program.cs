6196f1d27888b2343252bd42357b368f9b99527e
Павел Кабаков <pavelkabakov@gmail.com>
2023-04-23 21:57:20 +0300

    add task_66

--- task_64/Program.cs
+++ task_64/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите число:");
+int number1 = DataInput();
+Console.WriteLine($"Вы ввели число:{number1}, ряд натуральных чисел от {number1} до 1:");
+
+PrintNaturalNumber(number1);
+
+// Console.WriteLine(result);
+
+// Рекурсивный метод вывода
+void PrintNaturalNumber(int a, int b = 1)
+{
+    if (a < b) { return; }
+    {
+        Console.Write($"{a}, ");
+        a--;
+
+        PrintNaturalNumber(a);
+    }
+}
+
+// Рекурсивный метод проверки правильности ввода значения в консоль
+int DataInput()
+{
+
+    bool status = int.TryParse(Console.ReadLine(), out int a);
+    if (status) { return a; }
+
+    if (!status)
+    {
+        Console.WriteLine("Введено не число, повторите ввод");
+    }
+
+    return DataInput();
+
+}
--- task_66/Program.cs
+++ task_66/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите начало диапазона натуральных чисел M:");
+int a = DataInput();
+
+Console.Write("Введите конец диапазона натуральных чисел N:");
+int b = DataInput();
+
+int result = NaturalsNumbersSumm(a, b);
+
+Console.WriteLine($"сумма натуральных элементов в промежутке от M до N = {result}");
+
+// Рекурсивный метод подсчета суммы натуральных элементов 
+int NaturalsNumbersSumm(int a, int b, int summ = 0)
+{
+    if (b < a) { return summ; }
+    summ += b;
+    b--;
+
+    return NaturalsNumbersSumm(a, b, summ);
+
+}
+
+// Рекурсивный метод проверки правильности ввода значения в консоль
+int DataInput()
+{
+
+    bool status = int.TryParse(Console.ReadLine(), out int a);
+    if (status) { return a; }
+
+    if (!status)
+    {
+        Console.WriteLine("Введено не число, повторите ввод");
+    }
+
+    return DataInput();
+
+}
